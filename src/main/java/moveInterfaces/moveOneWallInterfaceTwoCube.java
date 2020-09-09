4d454a940b0b08f1d22617d0f1d36c9a342163ad
Filip <fzyla99@gmail.com>
2020-09-09 22:34:08 +0200

    Commit

--- src/main/java/moveInterfaces/moveOneWallInterfaceTwoCube.java
+++ src/main/java/moveInterfaces/moveOneWallInterfaceTwoCube.java
@@ -1,16 +1,13 @@
 package moveInterfaces;
 
-public interface moveOneWallInterface {
+public interface moveOneWallInterfaceTwoCube {
 
     void move_U(int rotate);
     void move_D(int rotate);
-    void move_E(int rotate);
 
     void move_R(int rotate);
     void move_L(int rotate);
-    void move_M(int rotate);
 
     void move_F(int rotate);
     void move_B(int rotate);
-    void move_S(int rotate);
 }
