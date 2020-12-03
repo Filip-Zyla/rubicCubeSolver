aafb0cef67d86aabda53d82ab97a93aedbd735ae
Filip Żyła <fzyla99@gmail.com>
2020-12-03 20:59:27 +0100

    another commit

--- src/main/java/graphicalMenu/graphMenu.java
+++ src/main/java/graphicalMenu/graphMenu.java
@@ -4,19 +4,26 @@ import cubes.Cube_2x2;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class graphMenu extends JComponent {
+public class graphMenu extends JComponent implements ActionListener {
 
     Cube_2x2 cube;
+    JButton b1,b2,b3,b4,b5;
+    JTextField tf;
+    JLabel jl;
+    JFrame window;
+    JPanel jp;
     public graphMenu(Cube_2x2 cube) {
         this.cube=cube;
-        JFrame window = new JFrame();
-        window.setSize(1500, 1000);
+        window = new JFrame();
+        window.setSize(1350, 650);
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        jp = new JPanel();
         for (int j = 0; j < 8; j++) {
             for (int i = 0; i < 6; i++) {
-                JPanel jp = new JPanel();
                 jp.setBounds(j * 100, i * 100, 100, 100);
                 switch (cube.getCube()[i][j]) {
                     case 0 -> jp.setBackground(Color.WHITE);
@@ -31,32 +38,84 @@ public class graphMenu extends JComponent {
             }
         }
 
-        JButton b1=new JButton("Reset cube");
+        b1=new JButton("Reset cube");
         b1.setBounds(850,100,140, 40);
         window.add(b1);
+        b1.addActionListener(this);
 
-        JButton b2=new JButton("Scramble");
+        b2=new JButton("Scramble");
         b2.setBounds(850,200,140, 40);
         window.add(b2);
+        b2.addActionListener(this);
 
-        JButton b3=new JButton("Move");
+        jl = new JLabel("Scramble");
+        jl.setBounds(1000,200,250, 40);
+        window.add(jl);
+
+        b3=new JButton("Move");
         b3.setBounds(850,300,140, 40);
         window.add(b3);
+        b3.addActionListener(this);
 
-        JTextField tf = new JTextField();
+        tf = new JTextField();
         tf.setBounds(1000, 300, 200, 40);
         window.add(tf);
+        tf.addActionListener(this);
 
-        JButton b4=new JButton("Solve cube");
+        b4=new JButton("Solve cube");
         b4.setBounds(850,400,140, 40);
         window.add(b4);
+        b4.addActionListener(this);
 
-        JButton b5=new JButton("Exit");
+        b5=new JButton("Exit");
         b5.setBounds(850,500,140, 40);
         window.add(b5);
+        b5.addActionListener(this);
 
         window.setLayout(null);
         window.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource()==b1){
+            cube = new Cube_2x2();
+        }else if(e.getSource()==b2){
+            String sc = Cube_2x2.randomScramble();
+            cube.moveCube(sc);
+            jl.setText(sc);
+
+            jp.repaint();
+
+        }else if(e.getSource()==b3){
+            String sc = tf.getText();
+            cube.moveCube(sc);
+        }else if(e.getSource()==b4){
+            cube.solve();
+        }else if(e.getSource()==b5){
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+        jp = new JPanel();
+        for (int j = 0; j < 8; j++) {
+            for (int i = 0; i < 6; i++) {
+                jp.setBounds(j * 100, i * 100, 100, 100);
+                switch (cube.getCube()[i][j]) {
+                    case 0 -> jp.setBackground(Color.WHITE);
+                    case 1 -> jp.setBackground(Color.RED);
+                    case 2 -> jp.setBackground(Color.BLUE);
+                    case 3 -> jp.setBackground(Color.GREEN);
+                    case 4 -> jp.setBackground(Color.getHSBColor(0.10f, 1f, 1f));
+                    case 5 -> jp.setBackground(Color.YELLOW);
+                    default -> jp.setBackground(Color.BLACK);
+                }
+                window.add(jp);
+            }
+        }
     }
 }
 
